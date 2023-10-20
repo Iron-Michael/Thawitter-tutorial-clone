2c9a961a0edac118f676eb24fc7f9efd99b54b46
Michael <sitthisaklo@kkumail.com>
2023-10-20 22:16:13 +0700

    fix-default-export-attention

--- pages/api/users/index.ts
+++ pages/api/users/index.ts
@@ -2,7 +2,10 @@ import { NextApiRequest, NextApiResponse } from "next";
 
 import prisma from "@/libs/prismadb";
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   if (req.method !== "GET") {
     return res.status(405).end();
   }

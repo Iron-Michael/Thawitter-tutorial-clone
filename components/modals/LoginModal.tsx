458b6e25bc2296e48fa53550f5b75654e958f36a
Michael <sitthisaklo@kkumail.com>
2023-10-20 15:07:45 +0700

    fix-error-after-deploy-first-time

--- components/modals/LoginModal.tsx
+++ components/modals/LoginModal.tsx
@@ -1,41 +1,44 @@
-import useLoginModal from "@/hooks/useLoginModal";
+import { signIn } from "next-auth/react";
 import { useCallback, useState } from "react";
+import { toast } from "react-hot-toast";
+
+import useLoginModal from "@/hooks/useLoginModal";
+import useRegisterModal from "@/hooks/useRegisterModal";
+
 import Input from "../Input";
 import Modal from "../Modal";
-import useRegisterModal from "@/hooks/useRegisterModal";
-import { signIn } from "next-auth/react";
 
 const LoginModal = () => {
   const loginModal = useLoginModal();
-  const RegisterModal = useRegisterModal();
+  const registerModal = useRegisterModal();
+
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [isLoading, setIsLoading] = useState(false);
 
-  const onToggle = useCallback(() => {
-    if (isLoading) {
-      return;
-    }
-
-    loginModal.onClose();
-    RegisterModal.onOpen();
-  }, [isLoading, RegisterModal, loginModal]);
-
   const onSubmit = useCallback(async () => {
     try {
       setIsLoading(true);
+
       await signIn("credentials", {
         email,
         password,
       });
 
+      toast.success("Logged in");
+
       loginModal.onClose();
     } catch (error) {
-      console.log(error);
+      toast.error("Something went wrong");
     } finally {
       setIsLoading(false);
     }
-  }, [loginModal, email, password]);
+  }, [email, password, loginModal]);
+
+  const onToggle = useCallback(() => {
+    loginModal.onClose();
+    registerModal.onOpen();
+  }, [loginModal, registerModal]);
 
   const bodyContent = (
     <div className="flex flex-col gap-4">
@@ -47,8 +50,8 @@ const LoginModal = () => {
       />
       <Input
         placeholder="Password"
-        onChange={(e) => setPassword(e.target.value)}
         type="password"
+        onChange={(e) => setPassword(e.target.value)}
         value={password}
         disabled={isLoading}
       />
@@ -56,19 +59,24 @@ const LoginModal = () => {
   );
 
   const footerContent = (
-    <div className=" text-neutral-400  text-center mt-4">
+    <div className="text-neutral-400 text-center mt-4">
       <p>
-        don't have an account?
+        do you wanna have an account of Thawitter
         <span
           onClick={onToggle}
-          className="text-white cursor-pointer hover:underline"
+          className="
+            text-white 
+            cursor-pointer 
+            hover:underline
+          "
         >
           {" "}
-          Create Account
+          Create an account
         </span>
       </p>
     </div>
   );
+
   return (
     <Modal
       disabled={isLoading}
